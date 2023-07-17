a94fe63f53e0ae615ef3f1c5844f503a38490d9a
Hysen <hysen.gashi42@gmail.com>
2023-07-17 19:08:18 +0200

    rregullimi i doctors foreach loop per leximin ne databaze dhe shfaqen ne web, si dhe ndryshim nese eshte admin nuk mundet me hy te webfaqja normal vetem e bon redirect ne panel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Doctor;
 
 // uuse App\Models\User;
 
@@ -14,7 +15,8 @@ class HomeController extends Controller
         {
             if(Auth::user()->usertype=='0')
             {
-                return view('user.home');
+                $doctor = Doctor::all();
+                return view('user.home',compact('doctor'));
             }
             else
             {
@@ -27,7 +29,16 @@ class HomeController extends Controller
         }
     }
 
-    public function index(){
-        return view('user.home');
+    public function index()
+    {
+        if(Auth::id())
+        {
+            return redirect('home');
+        }
+        else
+        {
+        $doctor = Doctor::all();
+        return view('user.home',compact('doctor'));
+    }
     }
 }
